c7fa2fc73a81b9050a90eddb736e7719b909a9d2
Saif-09 <saifmd238@gmail.com>
2023-08-31 04:21:48 +0530

    Error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 
 const ErrorPage = () => {
-  return (
-    <div>
-        <main>
-            <h1 >404</h1><br/>
-        </main>
-      
-    </div>
-  )
+    const navigate = useNavigate();
+    useEffect(() => {
+        setTimeout(() => {
+            alert("Redirecting back");
+            //it will redirect to 1 page above 
+            navigate(-1)
+        })
+    }, [])
+    return (
+        <div>
+            <main>
+                <h1 >404</h1><br />
+            </main>
+
+        </div>
+    )
 }
 
 export default ErrorPage
